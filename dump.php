4f65bc0ddab6451d82fedb992575a44a0affae9e
Charles Severance <csev@umich.edu>
2015-05-06 06:01:26 -0400

    Move the dump into the main index.php.

--- dump.php
+++ dump.php
@@ -13,36 +13,10 @@ if ( isset($_POST['url']) ) {
 
 echo("File length:".strlen($png)."\n");
 
-// Read the magic bytes and verify
-$retval = substr($png,0,8);
-$ipos = 8;
-if ($retval != "\x89PNG\x0d\x0a\x1a\x0a")
-    die('This is not a valid PNG image');
+$sections = extractBadgeInfo($png, 'openbadges', true);
 
-echo("\nSearching for PNG chunks...\n");
-// Loop through the chunks. Byte 0-3 is length, Byte 4-7 is type
-$chunkHeader = substr($png,$ipos,8);
-$ipos = $ipos + 8;
-while ($chunkHeader) {
-    // Extract length and type from binary data
-    $chunk = @unpack('Nsize/a4type', $chunkHeader);
-	    echo("\n".htmlentities($chunk['type'])." (".$chunk['size'].")\n");
-    $skip = false;
-    if ( $chunk['type'] == 'tEXt' ) {
-        $data = substr($png,$ipos,$chunk['size']);
-        $sections = explode("\0", $data);
-        print_r($sections);
-        // if ( $sections[0] == $key ) $skip = true;
-    }
-
-    // Extract the data and the CRC
-    $data = substr($png,$ipos,$chunk['size']+4);
-    $ipos = $ipos + $chunk['size'] + 4;
-
-    // Add in the header, data, and CRC
-    if ( ! $skip ) $retval = $retval . $chunkHeader . $data;
-
-    // Read next chunk header
-    $chunkHeader = substr($png,$ipos,8);
-    $ipos = $ipos + 8;
+if ( is_string($sections) ) {
+    echo($sections."\n");
+} else {
+    echo("Badge url: ".$sections[1]."\n");
 }
--- index.php
+++ index.php
@@ -4,16 +4,21 @@ require("util.php");
 
 $encrypted = false;
 $email = '';
+$badge_url='';
+$ver_url = '';
+$recepient = '';
+$salt = '';
+$ver_email = '';
+$baseUrl = str_replace("index.php","",curPageUrl());
 if ( isset($_GET['email']) ) {
     $email = $_GET['email'];
     $encrypted = bin2hex(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($PASSWORD), $_GET['email'], MCRYPT_MODE_CBC, md5(md5($PASSWORD))));  
+    $badge_url = $baseUrl . $encrypted . '.png';
 }
 ?>
 <html>
 <head>
 <title>The World's Easiest Mozilla Open Badge Maker (Beta)</title>
-<!-- https://github.com/mozilla/openbadges/wiki/Issuer-API -->
-<script src="http://beta.openbadges.org/issuer.js"></script>
 </head>
 <body style="font-family:sans-serif;">
 <h2>Welcome to the World's Easiest-To-Earn Mozilla Open Badge (Beta)</h2>
@@ -34,12 +39,39 @@ for <?php echo(htmlspecialchars($_GET['email'])); ?>
 display it on your 
 web site or maually upload it to a badge store.
 </p>
-<?php } ?>
+<?php } 
+
+if ( isset($_POST['url']) || isset($_FILES['upload']) ) {
+    echo("<pre>\n");
+    if ( isset($_FILES['upload']) && isset($_FILES['upload']['size']) &&
+        $_FILES['upload']['size'] > 0 ) {
+        $png = file_get_contents($_FILES['upload']['tmp_name']);
+        echo("Uploaded File length:".strlen($png)."\n");
+    } else {
+        $badge_url=$_POST['url'];
+        echo("Retrieving ".$_POST['url']."\n...\n");
+        $png = urlGET($_POST['url']);
+        echo("Retrieved ".strlen($png)." bytes\n");
+    }
+
+    $sections = extractBadgeInfo($png);
+
+    if ( is_string($sections) ) {
+        echo('Error in badge metadata: '.$sections."\n");
+    } else {
+        $ver_url = $sections[1];
+        echo("Badge assertion url:\n".$sections[1]."\n");
+    }
+    echo("</pre>\n");
+}
+?>
+
+
 <p>
-<form method="post" action="dump.php" 
-    enctype="multipart/form-data" target="_blank">
+<form method="post" enctype="multipart/form-data">
 Badge URL to Retrieve and Dump:<br/>
-<input type="text" name="url" size="90"><br>
+<input type="text" name="url" size="90"
+value="<?= htmlentities($badge_url) ?>"><br/>
 Badge File to Dump:
 <input type="file" name="upload"><br/>
 <input type="submit" value="Retrieve Badge Metadata">
@@ -49,13 +81,9 @@ Once you dump the badge metadata, you will see an assertion URL,
 which can be used as an assertion URL below.
 <p>
 <?php
-$ver_url = '';
-$recepient = '';
-$salt = '';
-$val_email = '';
-if ( isset($_POST['ver_url']) && isset($_POST['val_email']) ) {
+if ( isset($_POST['ver_url']) && isset($_POST['ver_email']) ) {
     $ver_url = $_POST['ver_url'];
-    $val_email = $_POST['val_email'];
+    $ver_email = $_POST['ver_email'];
     $response = urlGET($ver_url);
     $json = json_decode($response);
     if ( json_last_error() != JSON_ERROR_NONE ) {
@@ -66,10 +94,12 @@ if ( isset($_POST['ver_url']) && isset($_POST['val_email']) ) {
     // echo("\n<pre>\n");var_dump($json);echo("</pre>\n");
     $recepient = $json->recipient->identity;
     $salt = $json->recipient->salt;
-    $val_check = 'sha256$' . hash('sha256', $val_email . $salt);
+    $val_check = 'sha256$' . hash('sha256', $ver_email . $salt);
     echo('Validate Badge Contents: ');
     if ( $recepient == $val_check ) {
         echo(' <span style="color: green">Validated!</span>');
+    } else if ( strlen(trim($ver_email)) == 0 ) {
+        echo(' <span style="color: red">Missing Email Address</span>');
     } else {
         echo(' <span style="color: red">Validation Failure</span>');
     }
@@ -80,33 +110,39 @@ if ( isset($_POST['ver_url']) && isset($_POST['val_email']) ) {
         echo("<pre>\n");echo($response);echo("</pre>\n");
     }
 }
-
-if ( isset($_POST['recepient']) && isset($_POST['salt']) &&
-    isset($_POST['val_email']) ) {
-    $recepient = $_POST['recepient'];
-    $salt = $_POST['salt'];
-    $val_email = $_POST['val_email'];
-    $val_check = 'sha256$' . hash('sha256', $val_email . $salt);
-    echo('Validate Badge Contents: ');
-    if ( $recepient == $val_check ) {
-        echo(' <span style="color: green">Validated!</span>');
-    }
-    echo("<br>\n");
-    echo("Computed SHA: ".$val_check);
-    echo("<br>\n");
-}
 ?>
 <p>
 <form method="post">
 Verify A Badge Using Assertion URL:<br/>
 <input type="string" name="ver_url" size="80"
 value="<?= htmlentities($ver_url) ?>"><br/>
-Expected Email: 
-<input type="string" name="val_email" size="80"
-value="<?= htmlentities($val_email) ?>"><br/>
+Expected Email Address (required): 
+<input type="string" name="ver_email" size="40"
+value="<?= htmlentities($ver_email) ?>"><br/>
 <input type="submit" name="validate" value="Retrieve and Validate Assertion URL">
 </form>
 </p>
+<?php
+
+if ( isset($_POST['recepient']) && isset($_POST['salt']) &&
+    isset($_POST['ver_email']) ) {
+    $recepient = $_POST['recepient'];
+    $salt = $_POST['salt'];
+    $ver_email = $_POST['ver_email'];
+    $val_check = 'sha256$' . hash('sha256', $ver_email . $salt);
+    echo('Validate Badge Contents: ');
+    if ( $recepient == $val_check ) {
+        echo(' <span style="color: green">Validated!</span>');
+    } else if ( strlen(trim($ver_email)) == 0 ) {
+        echo(' <span style="color: red">Missing Email Address</span>');
+    } else {
+        echo(' <span style="color: red">Validation Failure</span>');
+    }
+    echo("<br>\n");
+    echo("Computed SHA: ".$val_check);
+    echo("<br>\n");
+}
+?>
 <p>
 Assertion Data:
 <form method="post">
@@ -116,9 +152,9 @@ value="<?= htmlentities($recepient) ?>"><br/>
 Salt: 
 <input type="string" name="salt" size="80"
 value="<?= htmlentities($salt) ?>"><br/>
-Expected Email: 
-<input type="string" name="val_email" size="80"
-value="<?= htmlentities($val_email) ?>"><br/>
+Expected Email Address (required): 
+<input type="string" name="ver_email" size="40"
+value="<?= htmlentities($ver_email) ?>"><br/>
 <input type="submit" name="validate" value="Validate Using Asserton Data">
 </form>
 </p>
@@ -136,9 +172,12 @@ The source code to this project is at
 Comments welcome.
 </p>
 <p>
-The OBE 1.0 Specification
+These badges meet the 
 <a href="https://github.com/mozilla/openbadges-specification/blob/master/Assertion/latest.md" 
-target="_blank">is here</a>.
+target="_blank">Open Badges 1.0 Specification</a>.
+The <a href="http://specification.openbadges.org/history/1.1.html"
+target="_blank">Open Badges 1.1 Specification</a> 
+extends the badge metadata formats using JSON-LD.
 <p>
 <a href="http://www.dr-chuck.com/">Dr. Chuck</a>
-Tue Mar 19 23:41:50 EDT 2013
+Wed May  6 05:54:18 EDT 2015
