8fc435a6a76c437b77e53b26c6e4cc0a7644784c
Charles Severance <csev@umich.edu>
2016-08-27 11:18:24 -0400

    A bit o debug

--- util.php
+++ util.php
@@ -1,6 +1,6 @@
 <?php 
 
-  function curPageURL() {
+function curPageURL() {
     $pageURL = (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on")
              ? 'http'
              : 'https';
@@ -9,10 +9,10 @@
     //$pageURL .= $_SERVER['REQUEST_URI'];
     $pageURL .= $_SERVER['PHP_SELF'];
     return $pageURL;
-  }
+}
 
 
-  function urlGET($url) {
+function urlGET($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -24,6 +24,11 @@
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
+    if(curl_error($ch))
+    {
+        echo "<pre>\n".'network error:' . curl_error($ch)."\n</pre>\n";
+    }
+
     return $response;
-  }
+}
 
